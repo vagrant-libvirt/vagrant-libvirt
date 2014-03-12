16ba363c6ca871be80e8540a08da2be599793655
Maximilian Fischer <mafi@inagile.org>
2014-03-12 12:48:56 +0000

    Add support for bridge interface

--- Gemfile
+++ Gemfile
@@ -7,10 +7,10 @@ group :development do
   # We depend on Vagrant for development, but we don't add it as a
   # gem dependency because we expect to be installed within the
   # Vagrant environment itself using `vagrant plugin`.
-  gem "vagrant", :git => "git://github.com/mitchellh/vagrant.git"
+  gem "vagrant", :git => "git://github.com/mitchellh/vagrant.git", :tag => "v1.4.3"
 end
 
 group :plugins do
-  gem "vagrant-libvirt", path: "."
+  gem "vagrant-mutate"
 end
 
--- lib/vagrant-libvirt/config.rb
+++ lib/vagrant-libvirt/config.rb
@@ -29,6 +29,9 @@ module VagrantPlugins
       attr_accessor :management_network_name
       attr_accessor :management_network_address
 
+      # Libvirt default network
+      attr_accessor :management_address
+
       # Default host prefix (alternative to use project folder name)
       attr_accessor :default_prefix
 
@@ -53,6 +56,7 @@ module VagrantPlugins
         @storage_pool_name = UNSET_VALUE
         @management_network_name    = UNSET_VALUE
         @management_network_address = UNSET_VALUE
+        @management_address = UNSET_VALUE
 
         # Domain specific settings.
         @memory            = UNSET_VALUE
@@ -76,6 +80,7 @@ module VagrantPlugins
         @storage_pool_name = 'default' if @storage_pool_name == UNSET_VALUE
         @management_network_name = 'vagrant-libvirt' if @management_network_name == UNSET_VALUE
         @management_network_address = '192.168.121.0/24' if @management_network_address == UNSET_VALUE
+        @management_address = nil if @management_address == UNSET_VALUE
 
         # Domain specific settings.
         @memory = 512 if @memory == UNSET_VALUE
