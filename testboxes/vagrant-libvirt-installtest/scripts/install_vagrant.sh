df59909c3d142e3345554e4dd254d1f75a433c35
Maximilian Fischer <mafi@inagile.org>
2014-05-28 00:11:15 +0200

    Add installation of vagrant mutate to provisioner

--- testboxes/vagrant-libvirt-installtest/scripts/install_vagrant.sh
+++ testboxes/vagrant-libvirt-installtest/scripts/install_vagrant.sh
@@ -5,3 +5,4 @@ wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb
 sudo dpkg -i /home/vagrant/vagrant_1.6.2_x86_64.deb
 sudo apt-get -y install build-essential
 sudo apt-get -y install libvirt-dev
+vagrant plugin install vagrant-mutate
