47a21433a5b215a67214c9688c3be5310e3bc2ea
Brian Pitts <brian@polibyte.com>
2014-12-07 12:16:05 -0600

    Add lock around storage pool creation; closes #278

--- lib/vagrant-libvirt/action/handle_storage_pool.rb
+++ lib/vagrant-libvirt/action/handle_storage_pool.rb
@@ -6,38 +6,42 @@ module VagrantPlugins
       class HandleStoragePool
         include VagrantPlugins::ProviderLibvirt::Util::ErbTemplate
 
+        @@lock = Mutex.new
+
         def initialize(app, env)
           @logger = Log4r::Logger.new("vagrant_libvirt::action::handle_storage_pool")
           @app = app
         end
 
         def call(env)
-          # Get config options.
-          config = env[:machine].provider_config
+          @@lock.synchronize do
+            # Get config options.
+            config = env[:machine].provider_config
 
-          # Check for storage pool, where box image should be created
-          fog_pool = ProviderLibvirt::Util::Collection.find_matching(
-            env[:libvirt_compute].pools.all, config.storage_pool_name)
-          return @app.call(env) if fog_pool
+            # Check for storage pool, where box image should be created
+            fog_pool = ProviderLibvirt::Util::Collection.find_matching(
+              env[:libvirt_compute].pools.all, config.storage_pool_name)
+            return @app.call(env) if fog_pool
 
-          @logger.info("No storage pool '#{config.storage_pool_name}' is available.")
+            @logger.info("No storage pool '#{config.storage_pool_name}' is available.")
 
-          # If user specified other pool than default, don't create default
-          # storage pool, just write error message.
-          raise Errors::NoStoragePool if config.storage_pool_name != 'default'
+            # If user specified other pool than default, don't create default
+            # storage pool, just write error message.
+            raise Errors::NoStoragePool if config.storage_pool_name != 'default'
 
-          @logger.info("Creating storage pool 'default'")
+            @logger.info("Creating storage pool 'default'")
 
-          # Fog libvirt currently doesn't support creating pools. Use
-          # ruby-libvirt client directly.
-          begin
-            libvirt_pool = env[:libvirt_compute].client.create_storage_pool_xml(
-              to_xml('default_storage_pool'))
-          rescue => e
-            raise Errors::CreatingStoragePoolError,
-              :error_message => e.message
+            # Fog libvirt currently doesn't support creating pools. Use
+            # ruby-libvirt client directly.
+            begin
+              libvirt_pool = env[:libvirt_compute].client.create_storage_pool_xml(
+                to_xml('default_storage_pool'))
+            rescue => e
+              raise Errors::CreatingStoragePoolError,
+                :error_message => e.message
+            end
+            raise Errors::NoStoragePool if !libvirt_pool
           end
-          raise Errors::NoStoragePool if !libvirt_pool
 
           @app.call(env)
         end
