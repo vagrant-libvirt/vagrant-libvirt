vagrant-libvirt/vagrant-libvirt

commit e904fd8941249f9fefdac6fc43df6a3457f93139

Enables setting of MAC address on management interface as per #426

lib/vagrant-libvirt/config.rb
@@ -47,6 +47,7 @@ module VagrantPlugins
       attr_accessor :management_network_name
       attr_accessor :management_network_address
       attr_accessor :management_network_mode
+      attr_accessor :management_network_mac
 
       # Default host prefix (alternative to use project folder name)
       attr_accessor :default_prefix
@@ -112,6 +113,7 @@ module VagrantPlugins
         @machine_virtual_size = UNSET_VALUE
         @disk_bus          = UNSET_VALUE
         @nic_model_type    = UNSET_VALUE
+        @management_network_mac          = UNSET_VALUE
         @nested            = UNSET_VALUE
         @volume_cache      = UNSET_VALUE
         @kernel            = UNSET_VALUE

lib/vagrant-libvirt/action/create_domain.rb
@@ -39,6 +39,7 @@ module VagrantPlugins
           @disk_bus = config.disk_bus
           @nested = config.nested
           @memory_size = config.memory.to_i * 1024
+          @management_network_mac = config.management_network_mac
           @domain_volume_cache = config.volume_cache
           @kernel = config.kernel
           @cmd_line = config.cmd_line
@@ -74,7 +75,7 @@ module VagrantPlugins
 
           # Get path to domain image from the storage pool selected if we have a box.
           if env[:machine].box
-            actual_volumes = 
+            actual_volumes =
               env[:machine].provider.driver.connection.volumes.all.select do |x|
                 x.pool_name == @storage_pool_name
               end
@@ -135,6 +136,7 @@ module VagrantPlugins
           env[:ui].info(" -- Domain type:       #{@domain_type}")
           env[:ui].info(" -- Cpus:              #{@cpus}")
           env[:ui].info(" -- Memory:            #{@memory_size / 1024}M")
+          env[:ui].info(" -- Management MAC:    #{@management_network_mac}")
           env[:ui].info(" -- Loader:            #{@loader}")
           if env[:machine].box
             env[:ui].info(" -- Base box:          #{env[:machine].box.name}")

lib/vagrant-libvirt/util/network_util.rb
@@ -10,7 +10,8 @@ module VagrantPlugins
         def configured_networks(env, logger)
           management_network_name = env[:machine].provider_config.management_network_name
           management_network_address = env[:machine].provider_config.management_network_address
-          management_network_mode = env[:machine].provider_config.management_network_mode 
+          management_network_mode = env[:machine].provider_config.management_network_mode
+          management_network_mac = env[:machine].provider_config.management_network_mac
           logger.info "Using #{management_network_name} at #{management_network_address} as the management network #{management_network_mode} is the mode"
 
           begin
@@ -36,6 +37,10 @@ module VagrantPlugins
             dhcp_enabled: true,
             forward_mode: management_network_mode,
           }
+
+          unless management_network_mac.nil?
+            management_network_options[:mac] = management_network_mac
+          end
 
           # add management network to list of networks to check
           networks = [ management_network_options ]

commit d60da562a567c0ad9dbc8b6e0cc3e00eae8069c2

Fix undefined method scan issue
management_network_address was defaulting to UNSET_VALUE which is of
type Object, thus management_network_address was always truthy.

This fixes issue introduced in
e904fd8941249f9fefdac6fc43df6a3457f93139.

lib/vagrant-libvirt/config.rb
@@ -84,10 +84,10 @@ module VagrantPlugins
 
       # Storage
       attr_accessor :disks
-  	  attr_accessor :cdroms
+      attr_accessor :cdroms
 
-	  # Inputs
-	  attr_accessor :inputs
+      # Inputs
+      attr_accessor :inputs
 
       def initialize
         @uri               = UNSET_VALUE
@@ -102,6 +102,7 @@ module VagrantPlugins
         @management_network_name    = UNSET_VALUE
         @management_network_address = UNSET_VALUE
         @management_network_mode = UNSET_VALUE
+        @management_network_mac  = UNSET_VALUE
 
         # Domain specific settings.
         @memory            = UNSET_VALUE
@@ -113,7 +114,6 @@ module VagrantPlugins
         @machine_virtual_size = UNSET_VALUE
         @disk_bus          = UNSET_VALUE
         @nic_model_type    = UNSET_VALUE
-        @management_network_mac          = UNSET_VALUE
         @nested            = UNSET_VALUE
         @volume_cache      = UNSET_VALUE
         @kernel            = UNSET_VALUE
@@ -311,6 +311,7 @@ module VagrantPlugins
         @management_network_name = 'vagrant-libvirt' if @management_network_name == UNSET_VALUE
         @management_network_address = '192.168.121.0/24' if @management_network_address == UNSET_VALUE
         @management_network_mode = 'nat' if @management_network_mode == UNSET_VALUE
+        @management_network_mac = nil if @management_network_mac == UNSET_VALUE
 
         # generate a URI if none is supplied
         @uri = _generate_uri() if @uri == UNSET_VALUE