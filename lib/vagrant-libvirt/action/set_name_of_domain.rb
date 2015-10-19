17c96f2a285ec6bc431f2d482bd49ad7535e752b
Jake Pelletier <jake.pelletier@sendgrid.com>
2015-10-19 12:57:34 -0600

    Update hash function, old one was in poor taste

--- lib/vagrant-libvirt/action/set_name_of_domain.rb
+++ lib/vagrant-libvirt/action/set_name_of_domain.rb
@@ -1,3 +1,4 @@
+require 'digest'
 module VagrantPlugins
   module ProviderLibvirt
     module Action
@@ -53,7 +54,7 @@ module VagrantPlugins
           domain_name << env[:machine].name.to_s
           domain_name.gsub!(/[^-a-z0-9_]/i, '')
           if config.random_hostname
-            domain_name_hash = domain_name.hash.to_s
+            domain_name_hash = Digest::MD5.hexdigest(domain_name)
             domain_name_hash << "_#{Time.now.utc.to_i}"
             domain_name = domain_name_hash
           end
