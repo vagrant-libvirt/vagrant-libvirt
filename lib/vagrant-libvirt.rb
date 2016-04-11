f8241645fcee4422f86a8761c8ddee0c2bdc4038
Gerben Meijer <infernix@gmail.com>
2016-04-11 16:02:42 +0200

    Must load vagrant before checking version

--- lib/vagrant-libvirt.rb
+++ lib/vagrant-libvirt.rb
@@ -13,6 +13,12 @@ module VagrantPlugins
   end
 end
 
+begin
+  require 'vagrant'
+rescue LoadError
+  raise 'The Vagrant Libvirt plugin must be run within Vagrant.'
+end
+
 # This is a sanity check to make sure no one is attempting to install
 # this into an early Vagrant version.
 if Vagrant::VERSION < '1.5.0'
