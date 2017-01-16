c065c1e6e523907f053468eefbd27534d737d7df
Dmitry Vasilets <pronix.service@gmail.com>
2017-01-16 16:40:23 +0100

    test1

--- lib/vagrant-libvirt.rb
+++ lib/vagrant-libvirt.rb
@@ -22,7 +22,7 @@ end
 # This is a sanity check to make sure no one is attempting to install
 # this into an early Vagrant version.
 if Vagrant::VERSION < '1.5.0'
-  raise 'The Vagrant Libvirt plugin is only compatible with Vagrant 1.5+'
+  raise 'The Vagrant Libvirt plugin is only compatible with Vagrant 1.5+.'
 end
 
 # make sure base module class defined before loading plugin
