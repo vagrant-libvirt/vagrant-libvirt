160d13325bb692b023a42918874c0c63e9058ce8
Zak B. Elep <zakame@zakame.net>
2019-02-27 10:35:43 +0800

    Allow customizing virt-sysprep operations via environment variable
    Instead of hard-coding sysprep operations, use an environment variable
    to set these instead.  Thanks @electrofelix for the suggestion!

--- lib/vagrant-libvirt/action/package_domain.rb
+++ lib/vagrant-libvirt/action/package_domain.rb
@@ -38,7 +38,8 @@ module VagrantPlugins
           `qemu-img rebase -p -b "" #{@tmp_img}`
           # remove hw association with interface
           # working for centos with lvs default disks
-          `virt-sysprep --no-logfile --operations defaults,-ssh-userdir,-ssh-hostkeys,-lvm-uuids -a #{@tmp_img}`
+          operations = ENV.get('VAGRANT_LIBVIRT_VIRT_SYSPREP_OPERATIONS', 'defaults,-ssh-userdir')
+          `virt-sysprep --no-logfile --operations #{operations} -a #{@tmp_img}`
           # add any user provided file
           extra = ''
           @tmp_include = @tmp_dir + '/_include'
