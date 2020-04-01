c553c02d62f32e7df12ac0054d83c074fe7adbe8
Thomas <thomas@drewermann.org>
2020-04-02 00:52:11 +0200

    add route option to networking config
    make it possible for guest specific templates to render routes

--- lib/vagrant-libvirt/action/create_network_interfaces.rb
+++ lib/vagrant-libvirt/action/create_network_interfaces.rb
@@ -207,7 +207,8 @@ module VagrantPlugins
                   type: :static,
                   ip: options[:ip],
                   netmask: options[:netmask],
-                  gateway: options[:gateway]
+                  gateway: options[:gateway],
+                  route: options[:route]
                 }.merge(network)
               else
                 network[:type] = :dhcp
