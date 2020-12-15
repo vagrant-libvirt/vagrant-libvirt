ff86cb9c273e3214bca3accfe90e992efe1964f5
Darragh Bailey <daragh.bailey@gmail.com>
2020-12-15 13:09:33 +0000

    restore coveralls coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   enable_coverage :branch if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5')
   add_filter 'spec/'
