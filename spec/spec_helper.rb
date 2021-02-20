80c0c8f11dec9380756bd7696228bf3bb78e403f
Darragh Bailey <daragh.bailey@gmail.com>
2021-02-20 13:04:36 +0000

    disable branch coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require 'coveralls'
 
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
-  enable_coverage :branch if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5')
   add_filter 'spec/'
 end
 
