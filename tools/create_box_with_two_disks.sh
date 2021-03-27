7612a6cfc6bcf9ef6fdff58d632ebf33fd9406c9
Richard Turc <>
2021-03-27 16:46:15 +0100

    WIP

--- tools/create_box_with_two_disks.sh
+++ tools/create_box_with_two_disks.sh
@@ -4,15 +4,12 @@ VAGRANT_CMD=${2:-vagrant}
 
 echo 'Create box with two disks'
 ${VAGRANT_CMD} box list
-${VAGRANT_CMD} box list --machine-readable
 if [ "$(${VAGRANT_CMD} box list | grep -c -E '^infernix/tinycore-two-disks\s')" -eq 0 ]
 then
     if [ "$(${VAGRANT_CMD} box list | grep -c -E '^infernix/tinycore\s')" -eq 0 ]
     then
         ${VAGRANT_CMD} box add infernix/tinycore
     fi
-    ls "${VAGRANT_HOME}/"
-    ls "${VAGRANT_HOME}/boxes/"
     NEW_PATH="${VAGRANT_HOME}/boxes/infernix-VAGRANTSLASH-tinycore-two-disks"
     cp -r "${VAGRANT_HOME}/boxes/infernix-VAGRANTSLASH-tinycore" "${NEW_PATH}"
     BOX_VERSION="$(${VAGRANT_CMD} box list --machine-readable | grep -A 10 infernix/tinycore-two-disks | grep box-version | cut -d, -f4)"
