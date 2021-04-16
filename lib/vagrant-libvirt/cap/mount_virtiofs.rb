e2766d563efc709a46a517972539c7c9e4faab8a
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-04-17 01:37:59 +0800

    Add `virtiofs` Support to vagrant-libvirt
    From <https://virtio-fs.gitlab.io/index.html#overview>:
    
    > Virtio-fs is a shared file system that lets virtual machines access a directory tree on the host. Unlike existing approaches, it is designed to offer local file system semantics and performance.
    
    From <https://virtio-fs.gitlab.io/index.html#faq>:
    
    > Existing solutions to this problem, such as virtio-9p, are based on existing network protocols that are not optimized for virtualization use cases. As a result they do not perform as well as local file systems and do not provide the semantics that some applications rely on.
    
    This PR add `virtiofs` support to vagrant-libvirt, which simply clone-and-hack from our existing 9p implementation. It also tidy up and synchronize naming for 9p implementation.
    
    Tested with:
    
      - Host: Ubuntu 20.10 + Linux 5.10.30 + QEMU 5.0.0 + Libvirt 6.6.0 + Vagrant 2.2.15
      - Guest: Ubuntu 20.04 + Linux 5.4.0
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- lib/vagrant-libvirt/cap/mount_virtiofs.rb
+++ lib/vagrant-libvirt/cap/mount_virtiofs.rb
@@ -4,10 +4,10 @@ require 'vagrant/util/retryable'
 module VagrantPlugins
   module ProviderLibvirt
     module Cap
-      class MountP9
+      class MountVirtioFS
         extend Vagrant::Util::Retryable
 
-        def self.mount_p9_shared_folder(machine, folders)
+        def self.mount_virtiofs_shared_folder(machine, folders)
           folders.each do |_name, opts|
             # Expand the guest path so we can handle things like "~/vagrant"
             expanded_guest_path = machine.guest.capability(
@@ -20,17 +20,12 @@ module VagrantPlugins
             # Mount
             mount_tag = Digest::MD5.new.update(opts[:hostpath]).to_s[0, 31]
 
-            mount_opts = '-o trans=virtio'
-            mount_opts += ",access=#{opts[:owner]}" if opts[:owner]
-            mount_opts += ",version=#{opts[:version]}" if opts[:version]
-            mount_opts += ",#{opts[:mount_opts]}" if opts[:mount_opts]
+            mount_opts = "-o #{opts[:mount_opts]}" if opts[:mount_opts]
 
-            mount_command = "mount -t 9p #{mount_opts} '#{mount_tag}' #{expanded_guest_path}"
+            mount_command = "mount -t virtiofs #{mount_opts} '#{mount_tag}' #{expanded_guest_path}"
             retryable(on: Vagrant::Errors::LinuxMountFailed,
                       tries: 5,
                       sleep: 3) do
-              machine.communicate.sudo('modprobe 9p')
-              machine.communicate.sudo('modprobe 9pnet_virtio')
               machine.communicate.sudo(mount_command,
                                        error_class: Vagrant::Errors::LinuxMountFailed)
             end
