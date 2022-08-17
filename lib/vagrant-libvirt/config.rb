vagrant-libvirt/vagrant-libvirt

commit 70866ecbaa8c4a68a94ebe77c44122c3a0e6736d

Flag when source device for public network does not exist (#1499)
Detect when the device provided for connecting the VM to a public
network via the host does not exist and attempt to provide a useful
error message to give end users a hint of how to correct.

Fixes: #1477

lib/vagrant-libvirt/config.rb
@@ -195,6 +195,9 @@ module VagrantPlugins
       # serial consoles
       attr_accessor :serials
 
+      # internal helper attributes
+      attr_accessor :host_device_exclude_prefixes
+
       def initialize
         @uri               = UNSET_VALUE
         @driver            = UNSET_VALUE
@@ -342,6 +345,9 @@ module VagrantPlugins
         @qemu_use_agent  = UNSET_VALUE
 
         @serials           = UNSET_VALUE
+
+        # internal options to help override behaviour
+        @host_device_exclude_prefixes = UNSET_VALUE
       end
 
       def boot(device)
@@ -972,6 +978,8 @@ module VagrantPlugins
         @qemu_use_agent = false if @qemu_use_agent == UNSET_VALUE
 
         @serials = [{:type => 'pty', :source => nil}] if @serials == UNSET_VALUE
+
+        @host_device_exclude_prefixes = ['docker', 'macvtap', 'virbr', 'vnet'] if @host_device_exclude_prefixes == UNSET_VALUE
       end
 
       def validate(machine)
@@ -1029,13 +1037,22 @@ module VagrantPlugins
           errors << "#{e}"
         end
 
-        machine.config.vm.networks.each do |_type, opts|
+        machine.config.vm.networks.each_with_index do |network, index|
+          type, opts = network
+
           if opts[:mac]
             if opts[:mac] =~ /\A([0-9a-fA-F]{12})\z/
               opts[:mac] = opts[:mac].scan(/../).join(':')
             end
             unless opts[:mac] =~ /\A([0-9a-fA-F]{2}:){5}([0-9a-fA-F]{2})\z/
               errors << "Configured NIC MAC '#{opts[:mac]}' is not in 'xx:xx:xx:xx:xx:xx' or 'xxxxxxxxxxxx' format"
+            end
+          end
+
+          # only interested in public networks where portgroup is nil, as then source will be a host device
+          if type == :public_network && opts[:portgroup] == nil
+            if !host_devices.include?(opts[:dev])
+              errors << "network configuration #{index} for machine #{machine.name} is a public_network referencing host device '#{opts[:dev]}' which does not exist, consider adding ':dev => ....' referencing one of #{host_devices.join(", ")}"
             end
           end
         end
@@ -1168,6 +1185,16 @@ module VagrantPlugins
           @proxy_command = proxy_command
         else
           @proxy_command = nil
+        end
+      end
+
+      def host_devices
+        @host_devices ||= begin
+          require 'socket'
+
+          Socket.getifaddrs.map { |iface| iface.name }.uniq.select do |dev|
+            dev != "lo" && !@host_device_exclude_prefixes.any? { |exclude| dev.start_with?(exclude) }
+          end
         end
       end
     end

spec/unit/config_spec.rb
@@ -630,6 +630,53 @@ describe VagrantPlugins::ProviderLibvirt::Config do
       end
     end
 
+    context 'with public_network defined' do
+      let(:host_devices) { [
+        instance_double(Socket::Ifaddr),
+        instance_double(Socket::Ifaddr),
+      ] }
+      before do
+        machine.config.vm.network :public_network, dev: 'eth0', ip: "192.168.2.157"
+        expect(Socket).to receive(:getifaddrs).and_return(host_devices)
+        expect(host_devices[0]).to receive(:name).and_return('eth0')
+        expect(host_devices[1]).to receive(:name).and_return('virbr0')
+      end
+
+      it 'should validate use of existing device' do
+        assert_valid
+      end
+
+      context 'when default device not on host' do
+        before do
+          machine.config.vm.network :public_network, dev: 'eno1', ip: "192.168.2.157"
+        end
+
+        it 'should be invalid' do
+          assert_invalid
+        end
+      end
+
+      context 'when using excluded host device' do
+        before do
+          machine.config.vm.network :public_network, dev: 'virbr0', ip: "192.168.2.157"
+        end
+
+        it 'should be invalid' do
+          assert_invalid
+        end
+
+        context 'when user overrides to allow device' do
+          before do
+            subject.host_device_exclude_prefixes = []
+          end
+
+          it 'should validate' do
+            assert_valid
+          end
+        end
+      end
+    end
+
     context 'with nvram defined' do
       before do
         subject.nvram = '/path/to/some/nvram'

commit 591e151b13f9b520618854a245af2489de677352

Use libvirt to query host physical devices (#1550)
Request the list of physical devices from libvirt when performing
config validation. This ensures remote instances using bridges will get
the correct list of devices as well as be alerted when the device needed
does not exist.

Fixes issue introduced with #1499

lib/vagrant-libvirt/config.rb
@@ -1067,8 +1067,9 @@ module VagrantPlugins
 
           # only interested in public networks where portgroup is nil, as then source will be a host device
           if type == :public_network && opts[:portgroup] == nil
-            if !host_devices.include?(opts[:dev])
-              errors << "network configuration #{index} for machine #{machine.name} is a public_network referencing host device '#{opts[:dev]}' which does not exist, consider adding ':dev => ....' referencing one of #{host_devices.join(", ")}"
+            devices = host_devices(machine)
+            if !devices.include?(opts[:dev])
+              errors << "network configuration #{index} for machine #{machine.name} is a public_network referencing host device '#{opts[:dev]}' which does not exist, consider adding ':dev => ....' referencing one of #{devices.join(", ")}"
             end
           end
         end
@@ -1210,11 +1211,9 @@ module VagrantPlugins
         end
       end
 
-      def host_devices
+      def host_devices(machine)
         @host_devices ||= begin
-          require 'socket'
-
-          Socket.getifaddrs.map { |iface| iface.name }.uniq.select do |dev|
+          machine.provider.driver.connection.client.list_all_interfaces().map { |iface| iface.name }.uniq.select do |dev|
             dev != "lo" && !@host_device_exclude_prefixes.any? { |exclude| dev.start_with?(exclude) }
           end
         end

spec/unit/config_spec.rb
@@ -635,13 +635,15 @@ describe VagrantPlugins::ProviderLibvirt::Config do
     end
 
     context 'with public_network defined' do
+      let(:libvirt_client)  { instance_double(::Libvirt::Connect) }
       let(:host_devices) { [
-        instance_double(Socket::Ifaddr),
-        instance_double(Socket::Ifaddr),
+        instance_double(Libvirt::Interface),
+        instance_double(Libvirt::Interface),
       ] }
       before do
         machine.config.vm.network :public_network, dev: 'eth0', ip: "192.168.2.157"
-        expect(Socket).to receive(:getifaddrs).and_return(host_devices)
+        expect(machine).to receive_message_chain('provider.driver.connection.client').and_return(libvirt_client)
+        expect(libvirt_client).to receive(:list_all_interfaces).and_return(host_devices)
         expect(host_devices[0]).to receive(:name).and_return('eth0')
         expect(host_devices[1]).to receive(:name).and_return('virbr0')
       end